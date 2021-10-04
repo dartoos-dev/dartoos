d0250bc5a30a3896f208677682090d96ca206456
rafael <r.amizes@gmail.com>
2021-10-04 19:07:55 -0300

    CI: fix build.yml so that it reports test coverage

--- lib/dartoos.dart
+++ lib/dartoos.dart
@@ -4,5 +4,6 @@
 library dartoos;
 
 export 'src/dartoos_base.dart';
+export 'src/future_wrap.dart';
 
-// TODO: Export any libraries intended for clients of this package.
+// @todo: #3 Export any libraries intended for clients of this package.
---
+++ lib/src/future_wrap.dart
@@ -0,0 +1,35 @@
+import 'dart:async';
+
+/// Represents a Future<T> value.
+
+/// Object-oriented approach: instead of returning a value,
+/// subclasses will themselves be the value.
+abstract class FutureWrap<T> implements Future<T> {
+  /// Main constructor.
+  FutureWrap(this._origin);
+
+  /// Creates a Future<T> from value.
+  FutureWrap.value(T value) : this(Future.value(value));
+
+  /// The encapsulated original Future.
+  final Future<T> _origin;
+
+  @override
+  Stream<T> asStream() => _origin.asStream();
+
+  @override
+  Future<T> catchError(Function onError, {bool Function(Object)? test}) =>
+      _origin.catchError(onError, test: test);
+
+  @override
+  Future<R> then<R>(FutureOr<R> Function(T) onValue, {Function? onError}) =>
+      _origin.then(onValue, onError: onError);
+
+  @override
+  Future<T> timeout(Duration timeLimit, {FutureOr<T> Function()? onTimeout}) =>
+      _origin.timeout(timeLimit, onTimeout: onTimeout);
+
+  @override
+  Future<T> whenComplete(FutureOr Function() action) =>
+      _origin.whenComplete(action);
+}
